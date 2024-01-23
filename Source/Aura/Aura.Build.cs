580240bccfd18db6f5b8fcb51ed50df035e56055
kitkaori <arthexgames@gmail.com>
2024-01-24 00:48:52 +0100

    Setup base ability system component and attribute set

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -2,15 +2,16 @@
 
 using UnrealBuildTool;
 
+
 public class Aura : ModuleRules
 {
 	public Aura(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", "GameplayTags", "GameplayTasks" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
